69d2d80baac771b2b2010bdbc3d7fe04cf8f43c7
Ryan Saucier <ryanyoubum@gmail.com>
2022-08-03 13:05:13 -0400

    db

--- models/index.js
+++ models/index.js
@@ -8,7 +8,7 @@ require("dotenv").config(); // Add this line
 let dbUrl =
   process.env.NODE_ENV === "production"
     ? process.env.MONGODB_URI
-    : "mongodb://127.0.0.1:27017/BowlingAlleyDB";
+    : "mongodb://127.0.0.1:27017/BowlingAlley";
 
 mongoose
   .connect(dbUrl)
